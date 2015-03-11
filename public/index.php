c268317300c377804d85b5a61cc6a8de0c297288
Ian Littman <ian@cloudyhills.com>
2015-03-11 16:16:42 -0500

    Do quick PHP -> Hack conversion

--- public/index.php
+++ public/index.php
@@ -1,66 +1,70 @@
-<?php
+<?hh
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\Slim(['templates.path' => '../templates']);
+use Aura\Sql\ExtendedPdoInterface as Database;
+use Services_Twilio as Twilio;
+use Slim\Slim as App;
+
+$app = new App(['templates.path' => '../templates']);
 
 $app->get('/', function() use ($app) {
     $app->render('home.php');
 });
 
 class RaffleService {
-    protected $db;
-    protected $twilio;
-    protected $phoneNumber;
+    protected Database $db;
+    protected Twilio $twilio;
+    protected string $phoneNumber;
 
-    public function __construct(\Aura\Sql\ExtendedPdoInterface $db, Services_Twilio $twilio, $phone_number) {
+    public function __construct(Database $db, Twilio $twilio, string $phone_number) {
         $this->db = $db;
         $this->twilio = $twilio;
         $this->phoneNumber = $phone_number;
     }
 
-    public function create($name, array $items) {
+    public function create(string $name, Vector<string> $items) : int {
         $sid = uniqid();
         $this->db->perform('INSERT INTO raffle (raffle_name, sid) VALUES(?, ?)', [$name, $sid]);
-        $id = $this->db->lastInsertId();
+        $id = (int) $this->db->lastInsertId();
         foreach ($items as $item)
             $this->db->perform('INSERT INTO raffle_item (raffle_id, item) VALUES(?, ?)', [$id, $item]);
         return $id;
     }
 
-    public function getEntrantCount($id) {
-        return $this->db->fetchValue('SELECT COUNT(*) FROM entrant WHERE raffle_id = ?', [$id]);
+    public function getEntrantCount(int $id) : int {
+        return (int) $this->db->fetchValue('SELECT COUNT(*) FROM entrant WHERE raffle_id = ?', [$id]);
     }
 
-    public function getPhoneNumber($id) {
+    public function getPhoneNumber(int $id) : string {
         return $this->phoneNumber;
     }
 
-    public function getCode($id) {
+    public function getCode(int $id) : int {
         return $id;
     }
 
-    public function isComplete($id) {
-        return $this->db->fetchValue('SELECT COUNT(*) FROM raffle WHERE is_complete = 1 && id = ?', [$id]);
+    public function isComplete(int $id) : bool {
+        return $this->db->fetchValue('SELECT COUNT(*) FROM raffle WHERE is_complete = 1 && id = ?', [$id]) > 0;
     }
 
-    public function getName($id) {
+    public function getName(int $id) : string {
         return $this->db->fetchValue('SELECT raffle_name FROM raffle WHERE id = ?', [$id]);
     }
 
-    public function getNameByCode($code) {
+    public function getNameByCode($code) : string {
         return $this->getName($code);
     }
 
-    public function getSid($id) {
+    public function getSid(int $id) : string {
         return $this->db->fetchValue('SELECT sid FROM raffle WHERE id = ?', [$id]);
     }
 
-    public function getEntrantPhoneNumbers($id) {
-        return $this->db->fetchCol('SELECT phone_number FROM entrant WHERE raffle_id = ?', [$id]);
+    public function getEntrantPhoneNumbers(int $id) : Vector<string> {
+        return new Vector($this->db->fetchCol('SELECT phone_number FROM entrant WHERE raffle_id = ?', [$id]));
     }
 
-    public function complete($id) {
+    public function complete(int $id) : Vector<string> {
         $this->db->perform('UPDATE raffle SET is_complete = 1 WHERE id = ?', [$id]);
 
         $items = $this->db->fetchCol('SELECT item FROM raffle_item WHERE raffle_id = ? ORDER BY id ASC', [$id]);
@@ -68,12 +72,12 @@ class RaffleService {
 
         shuffle($entrants);
 
-        $winnerNumbers = [];
+        $winnerNumbers = new Vector([]);
 
         foreach ($entrants as $k => $phone_number) {
             if (isset($items[$k])) {
                 $message = 'You won! Your prize: ' . $items[$k];
-                $winnerNumbers[] = $phone_number;
+                $winnerNumbers->add($phone_number);
             } else {
                 $message = 'Sorry, you didn\'t win this time. Maybe next time!';
             }
@@ -84,8 +88,8 @@ class RaffleService {
         return $winnerNumbers;
     }
 
-    public function recordEntry($code, $phone_number) {
-        if (!$this->raffleExists($code))
+    public function recordEntry(int $code, string $phone_number) : bool {
+        if (!$this->raffleExists((int) $code))
             return false;
         if ($this->db->fetchValue('SELECT COUNT(*) FROM entrant WHERE raffle_id = ? && phone_number = ?',
                 [$code, $phone_number]))
@@ -95,7 +99,7 @@ class RaffleService {
         return true;
     }
 
-    public function raffleExists($id) {
+    public function raffleExists(int $id) : bool {
         return $this->db->fetchValue('SELECT COUNT(*) FROM raffle WHERE id = ?', [$id]) > 0;
     }
 }
@@ -105,7 +109,7 @@ $rs = new RaffleService(
     new Services_Twilio($_SERVER['TWILIO_SID'], $_SERVER['TWILIO_TOKEN']), $_SERVER['TWILIO_NUMBER']
 );
 
-$isAuthorized = function($id) use ($app, $rs) {
+$isAuthorized = function(int $id) use ($app, $rs) {
     $sid = $rs->getSid($id);
     return $sid === $app->getCookie('sid' . $id) || $sid === $app->request()->get('sid');
 };
@@ -126,13 +130,15 @@ $app->post('/', function() use ($app, $rs) {
         return;
     }
 
-    $id = $rs->create($name, explode("\n", trim($items)));
+    $id = $rs->create($name, (new Vector(explode("\n", $items))));
 
     $app->setCookie('sid' . $id, $rs->getSid($id));
     $app->redirect('/' . $id);
 });
 
 $app->get('/:id', function($id) use ($app, $rs, $isAuthorized) {
+    $id = (int) $id;
+
     if (!$rs->raffleExists($id)) {
         $app->response()->setStatus(404);
         $app->render('not_found.php');
@@ -146,7 +152,7 @@ $app->get('/:id', function($id) use ($app, $rs, $isAuthorized) {
         $output['count'] = count($numbers);
 
         if ($isAuthorized($id))
-            $output['numbers'] = array_map(function($number) {return 'xxx-xxx-' . substr($number, -4);}, $numbers);
+            $output['numbers'] = $numbers->map(function($number) {return 'xxx-xxx-' . substr($number, -4);});
 
         echo json_encode($output);
         return;
@@ -166,9 +172,9 @@ $app->get('/:id', function($id) use ($app, $rs, $isAuthorized) {
 });
 
 $app->post('/twilio', function() use ($app, $rs) {
-    if ($rs->recordEntry($app->request()->post('Body'), $app->request->post('From'))) {
+    if ($rs->recordEntry((int) $app->request()->post('Body'), $app->request->post('From'))) {
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>
-            <Message>Your entry into " . $rs->getNameByCode($app->request->post('Body')) . " has been received!</Message>
+            <Message>Your entry into " . $rs->getNameByCode((int) $app->request->post('Body')) . " has been received!</Message>
         </Response>";
     } else {
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response />";
@@ -176,6 +182,8 @@ $app->post('/twilio', function() use ($app, $rs) {
 });
 
 $app->post('/:id', function($id) use ($app, $rs, $isAuthorized) {
+    $id = (int) $id;
+
     if (!$rs->raffleExists($id)) {
         $app->response()->setStatus(404);
         $app->render('not_found.php');
